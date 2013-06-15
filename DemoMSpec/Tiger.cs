1bfea38a22712986a5f5fbcbee0c74f8a7e7f275
Joe Leo <joseph.leo3@gmail.com>
2013-06-15 18:02:13 -0400

    iteration1

---
+++ DemoMSpec/Tiger.cs
@@ -0,0 +1,44 @@
+﻿using Machine.Specifications;
+
+#region ResharperDisable
+
+// ReSharper disable InconsistentNaming 
+// ReSharper disable CheckNamespace
+// ReSharper disable UnusedMember.Local
+
+#endregion
+namespace DemoMSpec
+{
+    public class Tiger
+    {
+        public string Poke(Poker poker)
+        {
+            if (poker.IsAnnoying())
+                return "Growl";
+            return "Purrrr";
+        }
+    }
+
+    public class Poker
+    {
+        public bool IsAnnoying()
+        {
+            return true;
+        }
+    }
+
+    [Subject(typeof (Tiger), "Hello World")]
+    class when_poking_the_tiger_with_your_finger
+    {
+        protected static Tiger Tiger;
+        protected static Poker Finger;
+
+        Establish context = () =>
+            {
+                Tiger = new Tiger();
+                Finger = new Poker();
+            };
+
+        It will_annoy_him = () => Tiger.Poke(Finger).ShouldEqual("Growl");
+    }
+}
